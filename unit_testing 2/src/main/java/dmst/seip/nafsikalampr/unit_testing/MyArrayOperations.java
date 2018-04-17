e4a1f5c14b5f0998b23790e86009cad381ca476c
Nafsika Lamprinopoulou <nafsikalbch1997@gmail.com>
2018-04-17 13:52:14 +0300

    Update MyArrayOperations.java
    add comments for MyMathOp

--- unit_testing 2/src/main/java/dmst/seip/nafsikalampr/unit_testing/MyArrayOperations.java
+++ unit_testing 2/src/main/java/dmst/seip/nafsikalampr/unit_testing/MyArrayOperations.java
@@ -4,20 +4,27 @@ import org.mockito.InjectMocks;
 
 public class MyArrayOperations {
 	
-    //getting ready for mocking
+    /**
+    * In this class we create a array from the txt elements in the file
+    * We use the readFile method in the MyFileUtilities to do that
+    */
+	
 	@InjectMocks
     private MyFileUtilities utils;
 	public Object gradeFrequencies;
+
+    /**
+    * Method counts how many time the grade appears in the txt and add it to frequency array
+    * @param filepath	the txt path 
+    * @param utils	the MyFileUtilities object we use to read the txt
+    */
 	
     public int[] gradeFrequencies(String filepath,MyFileUtilities utils) {
     	    
-            //using utils to call the readFile method for the specific filepath
     	    int[] array = utils.readFile(filepath);
 
-            //initializing return array
     	    int[] frequency =new int[array.length];
 
-            //loop to add elements
     	    for (int i=0; i< array.length ; i++) {
         	    if (array[i]==0) {
         	    	   frequency[0]++;
