bc30bd58e489590ebe59a717a9dd538db8415409
Nafsika Lamprinopoulou <nafsikalbch1997@gmail.com>
2018-04-17 14:00:56 +0300

    Update MyMath.java
    add documentation in MyMath

--- unit_testing 2/src/main/java/dmst/seip/nafsikalampr/unit_testing/MyMath.java
+++ unit_testing 2/src/main/java/dmst/seip/nafsikalampr/unit_testing/MyMath.java
@@ -1,10 +1,20 @@
 package dmst.seip.nafsikalampr.unit_testing;
 
+/**
+* Class contains division and sign reversion
+*/
+
 public class MyMath 
 {
     
+    /**
+    * method is a simple division with exception
+    * @param num the numinator
+    * @param denom the denominator
+    * @exception is thrown in case denom = 0
+    */
+    
     public double divide(int num, int denom) {
-      //checking the denom/ throwing exception
       if (denom == 0) {
         throw new IllegalArgumentException("Denom has to be other than 0");
     	}
@@ -12,7 +22,11 @@ public class MyMath
     	    
     }
     
-   	// return a number with reversed sign
+    /**
+    * method gets a number and reverses the sign unless num=0
+    *@param number
+    */
+    
    	public int reverseNumber(int number) {
    		if(number != 0) {
    		    return -number;
