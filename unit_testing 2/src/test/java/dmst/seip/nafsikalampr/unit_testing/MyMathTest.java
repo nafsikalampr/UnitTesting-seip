80f0d60156ecf05448ee9cf7f814dc973e952b45
Nafsika Lamprinopoulou <nafsikalbch1997@gmail.com>
2018-04-17 14:14:05 +0300

    Update MyMathTest.java
    add documentation for MyMathTest

--- unit_testing 2/src/test/java/dmst/seip/nafsikalampr/unit_testing/MyMathTest.java
+++ unit_testing 2/src/test/java/dmst/seip/nafsikalampr/unit_testing/MyMathTest.java
@@ -2,45 +2,63 @@ package dmst.seip.nafsikalampr.unit_testing;
 
 import org.junit.Test;
 import org.junit.Assert;
+
 /**
- * Unit test for simple MyMath.
+ * Unit test for MyMath class 
  */
+
 public class MyMathTest 
 {
     MyMath math = new MyMath();
     
-    //testing simple case
-	@Test
+    /**
+    * test for a simple division
+    */
+	
+    @Test
     public void testSimple()
     {
 	    Assert.assertEquals(2.0 , math.divide(6, 3),0);
     }
 	
-	//testing doubles
-	@Test
-	public void testDoubles() 
-	{
+    /**
+    * test in case number is a double
+    */
+	
+    @Test
+    public void testDoubles() 
+    {
 	    Assert.assertEquals(2.5, math.divide(5, 2),0);
-	}
+    }
     
-    //testing negative numbers
-	@Test
-	public void testNegative() 
-	{
+    /**
+    * test in case number is negative
+    */
+	
+    @Test
+    public void testNegative() 
+    {
 	Assert.assertEquals(-1.0, math.divide(-6, 6),0);
-	}
+    }
 	
-	//testing num=0    
-	@Test
-	public void testZeroNum()
-	{
-		Assert.assertEquals(0.0, math.divide(0, 3),0);
-	}
+    /**
+    * test in case numinator is zero
+    */
 	
-	//testing denom=0, has to throw exception
-	@Test(expected = IllegalArgumentException.class)
-	public void testZeroDenom()
-	{
-		math.divide(2, 0);
-	}
+    @Test
+    public void testZeroNum()
+    {
+	Assert.assertEquals(0.0, math.divide(0, 3),0);
+    }
+	
+    /**
+    * test in case denominator is 0 has to throw an 
+    * @exception
+    */    
+	
+    @Test(expected = IllegalArgumentException.class)
+    public void testZeroDenom()
+    {
+	math.divide(2, 0);
+    }
 }
