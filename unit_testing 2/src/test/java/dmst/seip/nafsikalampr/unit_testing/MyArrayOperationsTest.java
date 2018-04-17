3cd8a61869c04cf17e38647d392e258b92cba071
Nafsika Lamprinopoulou <nafsikalbch1997@gmail.com>
2018-04-17 14:06:18 +0300

    Update MyArrayOperationsTest.java
    add documentation for myArrayOpTest

--- unit_testing 2/src/test/java/dmst/seip/nafsikalampr/unit_testing/MyArrayOperationsTest.java
+++ unit_testing 2/src/test/java/dmst/seip/nafsikalampr/unit_testing/MyArrayOperationsTest.java
@@ -3,20 +3,32 @@ package dmst.seip.nafsikalampr.unit_testing;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+/**
+* class uses mocking to test MyArrayOperations
+*/
 
 public class MyArrayOperationsTest {
 	
 	MyArrayOperations mao;
 	
+	/**
+	* class constructor
+	*/
+	
 	public MyArrayOperationsTest() {
 		this.mao= new MyArrayOperations();
 	}
 	
-	//mocking with specific txt
+	/**
+	* Test that mocks and predifines the results of MyArrayOp
+	*/
+	
 	@Test
 	public void testMyArrayOpMock(){
 	    MyArrayOperations mao=mock(MyArrayOperations.class);
+	    //pre-define gradeFrequencies result
 	    when( mao.gradeFrequencies("src/test/resources/grades.txt", null)).thenReturn(new int[] {1,1,1,1,2,1});
+	    //call gradeFrequencies() for the mocked instance
 	    Assert.assertArrayEquals(new int[] {1,1,1,1,2,1}, mao.gradeFrequencies("src/test/resources/grades.txt", null));
 	}
 }
